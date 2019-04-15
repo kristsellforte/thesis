3de5750a1c2104ac56a8e3123bd892b8e11cfd7d
kristsellforte <krists@sellforte.com>
2019-04-15 16:40:27 +0300

    Add validation task

---
+++ airflow/dags/launcher/launcher.py
@@ -0,0 +1,35 @@
+import docker
+
+from docker import Client
+
+log = logging.getLogger(__name__)
+
+
+def launch_docker_container(**context):
+    image_name = context['image_name']
+    client: Client = docker.from_env()
+
+    log.info(f"Creating image {image_name}")
+    container = client.create_container(image=image_name)
+
+    container_id = container.get('Id')
+    log.info(f"Running container with id {container_id}")
+    client.start(container=container_id)
+
+    logs = client.logs(container_id, follow=True, stderr=True, stdout=True, stream=True, tail='all')
+
+    try:
+        while True:
+            l = next(logs)
+            log.info(f"Task log: {l}")
+    except StopIteration:
+        pass
+        
+    inspect = client.inspect_container(container)
+    log.info(inspect)
+    if inspect['State']['ExitCode'] != 0:
+                raise Exception("Container has not finished with exit code 0")
+
+    log.info(f"Task ends!")
+    my_id = context['my_id']
+    context['task_instance'].xcom_push('data', f'my name is {my_id}', context['execution_date'])
